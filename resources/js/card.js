952a26c6821102b810bd82e843417abf2c28570e
Alies Lapatsin <web.lapateen@gmail.com>
2022-05-13 14:40:44 +0400

    Apply minor stylefixes

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,5 +1,6 @@
-import Card from './components/Card'
+/* global Nova */
+import Card from './components/Card';
 
 Nova.booting((app, store) => {
-  app.component('html-card', Card)
-})
+    app.component('html-card', Card)
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,13 @@
-let mix = require('laravel-mix')
+// eslint-disable-next-line no-undef
+const mix = require('laravel-mix');
 
-require('./nova.mix')
+require('./nova.mix');
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/card.js', 'js')
-  .vue({ version: 3 })
-  .nova('interaction-design-foundation/nova-html-card')
+    .setPublicPath('dist')
+    .js('resources/js/card.js', 'js')
+    .vue({version: 3})
+    .nova('interaction-design-foundation/nova-html-card')
 
 if (mix.inProduction()) {
     mix.version();
