a20ae46ffd63a307eb9612e1b9717847c6efcc4e
Jason Tame <jason@team.interaction-design.org>
2022-12-30 16:43:42 +0200

    Add card content component

---
+++ resources/js/components/CardContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-html="content" class="htmlCard__content"></div>
+</template>
+
+<script>
+export default {
+  props: ['content'],
+};
+</script>
+
+<style lang="scss">
+.htmlCard__content {
+  p:not(:last-child) {
+    margin-bottom: 1em;
+  }
+}
+</style>
