20bc3c98c85129fb4636b37834159f3742c6090e
Alies Lapatsin <5278175+alies-dev@users.noreply.github.com>
2025-09-11 07:06:09 +0000

    Refactor test attributes to use PHP 8 attributes for better readability

--- tests/CardServiceProviderTest.php
+++ tests/CardServiceProviderTest.php
@@ -5,10 +5,10 @@ namespace InteractionDesignFoundation\HtmlCard\Tests;
 use Illuminate\Support\Facades\App;
 use InteractionDesignFoundation\HtmlCard\MarkdownConverter;
 
-/** @covers \InteractionDesignFoundation\HtmlCard\CardServiceProvider */
+#[\PHPUnit\Framework\Attributes\CoversClass(\InteractionDesignFoundation\HtmlCard\CardServiceProvider::class)]
 final class CardServiceProviderTest extends TestCase
 {
-    /** @test */
+    #[\PHPUnit\Framework\Attributes\Test]
     public function it_registers_markdown_converter_implementation(): void
     {
         $converter = App::make(MarkdownConverter::class);
